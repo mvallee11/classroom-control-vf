77c47e8422427c0dd61bf457f28d192d34292ae8
mvallee11 <mvallee@costco.com>
2016-05-24 07:01:08 -0700

    Update site.pp
    Added motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,13 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
+
+file { 'motd':
+  path    => '/etc/motd',
+  ensure  => file,
+  owner   => root,
+  group   => root,
+  mode    => '0664'
+  content => 'Isn't Puppet fun?'
+}
 }
