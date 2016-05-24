df2e74915436a69ad883e9fd1121a87eaf83dd0f
mvallee11 <mvallee@costco.com>
2016-05-24 10:23:09 -0700

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,16 +1,16 @@
 class skeleton {
   file {'/etc/skel':
-  ensure  => directory,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-}
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  }
+  
   file {'/etc/skel/.bashrc':
     ensure  => file,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    source  => '/site/skeleton/files/bashrc',
-}
-
+    source  => 'puppet:///modules/skeleton/bashrc',
+  }
 }
