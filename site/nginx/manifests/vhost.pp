7c598d89c01284f4fe6bd9b6d023ccbb7654e3ec
mvallee11 <mvallee@costco.com>
2016-05-25 11:05:55 -0700

    Create vhost.pp

---
+++ site/nginx/manifests/vhost.pp
@@ -0,0 +1,10 @@
+#define nginx::vhost {
+#  $port       = '80',
+#  $servername = $title,
+#  $docroot    = "${nginx::docroot}/vhosts/${title}",
+#  {
+#  file { $docroot
+#    ensure => directory,
+#  }
+  
+#}
